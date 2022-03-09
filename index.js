39993ce4529d2a6acf3cac55bf4ee862449c8b6f
ROOHITH <rohith602@outlook.com>
2022-03-09 14:08:26 +0530

    commit http node app 1

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const { readFileSync } = require('fs');
 const http = require('http');
 const server = http.createServer((req,res)=>{
-    
+    console.log(req.url)
     if(req.url==="/"){
         res.writeHead(200,{'content-type':'text/html'});
-        res.write(readFileSync('./index.html'))
-        res.end("hello");
+        res.write(readFileSync('./html/index.html'))
+        res.end();
+    }
+    else if(req.url==="/css/index.css"){
+        res.writeHead(200,{'content-type':'text/css'});
+        res.write(readFileSync('./css/index.css'))
+        res.end();
+    }
+    else if(req.url==="/about"){
+        res.writeHead(200,{'content-type':'text/html'});
+        res.write(readFileSync('./html/about.html'))
+        res.end();
+    }
+    else{
+        res.writeHead(404,{'content-type':'text/html'})
+        res.write('<h1>not founf</h1>')
+        res.end()
     }
 })
 server.listen(5000);
